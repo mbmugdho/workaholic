3d4f80e1bbabefa29d2fc52dd1b46f442d624cfb
mbmugdho <mdmontasirbillah5@gmail.com>
2026-01-07 22:33:12 +0600

    centralize routes and wire protected dashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout'
-import DashboardLayout from './layouts/DashboardLayout'
-
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import DashboardEntry from './pages/dashboard/DashboardEntry'
+import AppRoutes from "./routes/Routes";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Route>
-
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route index element={<DashboardEntry />} />
-      </Route>
-    </Routes>
-  )
+  return <AppRoutes />;
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom'
 
-const Routes = () => {
+import MainLayout from '../layouts/MainLayout'
+import DashboardLayout from '../layouts/DashboardLayout'
+
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+import PrivateRoute from './PrivateRoute'
+import AdminRoute from './AdminRoute'
+import BuyerRoute from './BuyerRoute'
+import WorkerRoute from './WorkerRoute'
+
+import DashboardRedirect from '../pages/dashboard/DashboardRedirect'
+
+// Placeholder pages (we’ll replace with real dashboards later)
+import WorkerHome from '../pages/dashboard/worker/WorkerHome'
+import BuyerHome from '../pages/dashboard/buyer/BuyerHome'
+import AdminHome from '../pages/dashboard/admin/AdminHome'
+
+export default function AppRoutes() {
   return (
-    <div>
-      
-    </div>
-  );
-};
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
 
-export default Routes;
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <DashboardLayout />
+          </PrivateRoute>
+        }
+      >
+        <Route index element={<DashboardRedirect />} />
+
+        <Route
+          path="worker/home"
+          element={
+            <WorkerRoute>
+              <WorkerHome />
+            </WorkerRoute>
+          }
+        />
+
+        <Route
+          path="buyer/home"
+          element={
+            <BuyerRoute>
+              <BuyerHome />
+            </BuyerRoute>
+          }
+        />
+
+        <Route
+          path="admin/home"
+          element={
+            <AdminRoute>
+              <AdminHome />
+            </AdminRoute>
+          }
+        />
+      </Route>
+    </Routes>
+  )
+}
